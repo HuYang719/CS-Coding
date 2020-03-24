de3380e1d9bd4521abb249f158ee0592a488df7b
HuYang719 <646228006@qq.com>
2020-03-24 21:14:32 +0800

    solve L1011

---
+++ 01-Leetcode/L1011.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int D) {
+        int min_cap = 0;
+        int total_w = 0;
+        for(auto w:weights) {
+            total_w += w;
+            min_cap = max(min_cap, w);
+        }
+        
+        while(min_cap <total_w) {
+            int mid = min_cap + (total_w - min_cap)/2;
+    
+            int days = 0;
+            int current_w = 0;
+            for(auto w:weights) {
+                if(current_w + w> mid) {
+                    days++;
+                    current_w = 0;
+                }
+                current_w += w;
+            }
+            days++;
+            
+           // printf("Days = %d, l=%d, r= %d, mid = %d\n",days, min_cap, total_w, mid);
+            if(days > D)
+                min_cap = mid + 1;
+            else total_w = mid ;
+        }
+        return min_cap;
+        
+    }
+};
