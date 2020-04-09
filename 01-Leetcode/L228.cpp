176f77ae36f85ea2b82d1401d4812ebf8c24de69
HuYang719 <646228006@qq.com>
2020-04-09 11:06:24 +0800

    solve L228

---
+++ 01-Leetcode/L228.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        vector<string> res;
+        bool single = false;
+        if(nums.size() == 0) return {};
+        while(left < nums.size()-1){
+            string ans;
+            if(nums[left] != nums[left+1]-1) {
+                ans = to_string(nums[left]);
+                res.push_back(ans);
+                left++;
+                single = true;
+            }else{
+                int t = left;
+                while(left < nums.size()-1 && nums[left] == nums[left+1]-1)
+                    left++;
+                ans = to_string(nums[t])+"->"+to_string(nums[left]);
+                res.push_back(ans);
+                single = false;
+                left++;
+            }
+        }
+      // cout<<left<<endl;
+      if(left != nums.size())
+        res.push_back(to_string(nums.back()));
+      return res;
+    }
+};
