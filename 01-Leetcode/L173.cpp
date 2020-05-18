87c54372c1db1de885367945a3906d42ec4bef2a
HuYang719 <646228006@qq.com>
2020-05-18 14:07:36 +0800

    update L173

--- 01-Leetcode/L173.cpp
+++ 01-Leetcode/L173.cpp
@@ -49,3 +49,53 @@ public:
  * int param_1 = obj->next();
  * bool param_2 = obj->hasNext();
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> s;
+    void leftmostInorder(TreeNode* root) {
+        while(root != NULL) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+    BSTIterator(TreeNode* root) {
+        leftmostInorder(root);
+        
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* topmostNode = s.top();
+        s.pop();
+        if(topmostNode->right != NULL) {
+            leftmostInorder(topmostNode->right);
+        }
+        return topmostNode->val;
+        
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return s.size()>0;
+        
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
